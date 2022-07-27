2a95fd1ea4a5dbfe205e44d34f4b97e8f41bf729
qaziaqibnawaz <qazicse1@gmail.com>
2022-07-27 12:32:29 +0530

    changes

--- src/main/java/com/jenkins/page/test_SecondTestCase.java
+++ src/main/java/com/jenkins/page/test_SecondTestCase.java
@@ -1,7 +1,15 @@
 package com.jenkins.page;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class test_SecondTestCase {
+WebDriver driver;
 	
+	public test_SecondTestCase (WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
 	public void LastName()
 	{
 		String LastName="nawaz";
--- src/test/java/com/Jenkins/Integration/SecondTestCase.java
+++ src/test/java/com/Jenkins/Integration/SecondTestCase.java
@@ -2,13 +2,14 @@ package com.Jenkins.Integration;
 
 import org.testng.annotations.Test;
 
+import com.jenkins.lib.Baselib;
 import com.jenkins.page.test_SecondTestCase;
 
-public class SecondTestCase {
+public class SecondTestCase extends Baselib {
     @Test
 	public void secondTestCase()
 	{
-    	test_SecondTestCase obj=new test_SecondTestCase();
+    	test_SecondTestCase obj=new test_SecondTestCase(driver);
     	obj.LastName();
 	}
 }
